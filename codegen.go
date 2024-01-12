91fbe03094cbc21966399eaba4cbc400b747ed9c
Kevin Duffey <apifrst@gmail.com>
2024-01-11 23:15:17 -0800

    updated

--- codegen.go
+++ codegen.go
@@ -4,7 +4,8 @@ import (
 	"encoding/json"
 	"errors"
 	"github.com/extism/go-pdk"
-	t "github.com/spirefy/go-plugin-engine/types"
+	hostfuncs "github.com/spirefy/go-pdk"
+	t "github.com/spirefy/go-pdk/types"
 )
 
 //export register
@@ -18,12 +19,23 @@ func register() int32 {
 		Name:        "Codegen Source Loader ExtensionPoint",
 	}
 
+	l := t.Listener{
+		Event:    "load",
+		FuncName: "load",
+	}
+
+	l2 := t.Listener{
+		Event:    "load",
+		FuncName: "loadme",
+	}
+
 	p := &t.Plugin{
 		Id:              "spirefy.codegen",
 		Name:            "Spirefy Codegen",
 		Version:         "1.0.0",
 		Description:     "A plugin that provides code generation capabilities with additional extension points for other plugins to contribute to",
 		ExtensionPoints: []t.ExtensionPoint{ep},
+		Listeners:       []t.Listener{l, l2},
 	}
 
 	o, e := json.Marshal(p)
@@ -33,11 +45,37 @@ func register() int32 {
 		pdk.Output(o)
 	}
 
+	hostfuncs.CallExtension([]byte{})
 	return 0
 }
 
-func generate() {
-	pdk.Log(pdk.LogDebug, "Generating code")
+//export loadme
+func loadme() int32 {
+	pdk.Log(pdk.LogInfo, "Loading source for loadme extension event")
+
+	return 0
+}
+
+//export load
+func load() int32 {
+	e := make([]t.Extension, 0)
+	dta := pdk.Input()
+	if nil != dta && len(dta) > 0 {
+		err := json.Unmarshal(pdk.Input(), &e)
+
+		if nil != err {
+			pdk.SetError(err)
+			return 1
+		}
+	}
+
+	pdk.Log(pdk.LogInfo, "Loading source for extensions")
+
+	for _, e := range e {
+		pdk.Log(pdk.LogDebug, "Loading extension: "+e.Name)
+	}
+
+	return 0
 }
 
 //export handleEvent
